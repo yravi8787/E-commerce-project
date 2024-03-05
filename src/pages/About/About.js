25ce934eb435f094de02bca44dd6d566207bd984
yravi8787 <rvgkp9059@gmail.com>
2024-03-05 22:33:36 +0530

    first commit

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,7 +2,7 @@ import { ArrowBack } from "@mui/icons-material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../../Layout/Navbar/Navbar";
-import Mine from "../../Assets/Images/mine.jpg"
+
 
 const About = () => {
   const navigate = useNavigate();
@@ -18,17 +18,14 @@ const About = () => {
           <div className="row">
             <div className="col-lg-6 col-md-6 col-12">
               <p className="body1">
-                Hello, I'm Aman Gupta. I am Software Developer. As a Software
+                Hello, I'm Ravi Kumar Yadav. I am Software Developer. As a Software
                 developer, I have in-depth knowledge of Web development and App
                 Development. I have working experience of Web development. I
-                have in-depth knowledge of React js, Mongo Db, Express js, Next
-                js, HTML/CSS and javascript and as well as React native for App
-                development.
+                have in-depth knowledge of React js, Mongo Db, HTML/CSS and javascript 
+                and as well as React native for App development.
               </p>
             </div>
-            <div className="col-lg-6 col-md-6 col-12">
-              <img src={Mine} alt="mine" className="img-fluid" />
-            </div>
+            
           </div>
         </div>
       </div>
